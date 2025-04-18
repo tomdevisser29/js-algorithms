77843b2d9e15a857befdeffb9c223aa9f5ec5837
Tom de Visser <tomdevisser29@gmail.com>
2025-04-18 16:42:17 +0200

    Add the quicksort algorithm

---
+++ quicksort.js
@@ -0,0 +1,16 @@
+/**
+ * Quicksort is a faster sorting algorithm than selection sort, and uses divide
+ * and conquer. The base cases are an array with one or zero items. The
+ * recursive case is having unsorted sub-arrays of more than 2 items.
+ */
+function quicksort(array) {
+    if (array.length < 2) return array;
+
+    const pivot = array[0];
+    const less = array.slice(1).filter(i => i <= pivot);
+    const greater = array.slice(1).filter(i => i > pivot);
+
+    return quicksort(less).concat([pivot]).concat(quicksort(greater));
+}
+
+console.log(quicksort([3, 6, 2, 3, 6, 1, 5, 2, 4, 3]));
